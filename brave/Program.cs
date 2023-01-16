3db36aef5da044fa8f6a94accc80675ad4d1d877
SBuzmakov <105081120+SBuzmakov@users.noreply.github.com>
2023-01-16 16:44:12 +0300

    Update Program.cs

--- brave/Program.cs
+++ brave/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace brave
 {
@@ -10,30 +10,36 @@ namespace brave
             bool isPlaying = true;
             int gameScreenLength = 50;
             int gameScreenHeigth = 20;
-            int gameGroundHeigth = 3;
+            int gameGroundHeigth = 4;
             int playerPositionY = gameScreenHeigth - gameGroundHeigth - 1;
             int playerPositionX = 10;
+            int displayPositionX = 0;
+            int displayPositionY = 0;
             char[,] map = CreateObjects(gameScreenLength, gameScreenHeigth, gameGroundHeigth);
 
 
 
             while (isPlaying)
             {
-                DrawObjects(map, gameScreenLength, gameScreenHeigth, gameGroundHeigth, playerPositionX, playerPositionY);
+
+                DrawObjects(map, gameScreenLength, gameScreenHeigth, gameGroundHeigth, ref playerPositionX, ref playerPositionY, displayPositionX, displayPositionY);
 
                 ConsoleKeyInfo charKey = Console.ReadKey();
 
                 switch (charKey.Key)
                 {
-                    case ConsoleKey.D:
+                    case ConsoleKey.RightArrow:
                         map = GenerateNewObjects(map, gameGroundHeigth, playerPositionX, playerPositionY);
                         break;
                     case ConsoleKey.Enter:
-                        Shoot(map, playerPositionX, playerPositionY);
+                        Shoot(ref map, playerPositionX, playerPositionY, gameScreenLength, gameScreenHeigth, gameGroundHeigth,displayPositionX, displayPositionY);
                         break;
-                }
-                
-                Console.Clear();
+                    case ConsoleKey.Spacebar:
+                        Jump(ref map, playerPositionX, playerPositionY, gameScreenLength, gameScreenHeigth, gameGroundHeigth, displayPositionX, displayPositionY);
+                        break;
+                    default:
+                        break;
+                }               
             }           
         }
 
@@ -42,7 +48,7 @@ namespace brave
             if (objects[playerPositionY, playerPositionX + 1] != '#')
             {
                 Random random = new Random();
-                int erasingXNumber = 2;
+                int erasingXNumber = 1;
 
                 for (int y = 0; y < objects.GetLength(0) - ground; y++)
                 {
@@ -88,17 +94,22 @@ namespace brave
             {
                 for (int x = 0; x < objects.GetLength(1); x++)
                 {
-                    objects[y, x] = '#';
+                    objects[y, x] = 'M';
                 }
-
-                Console.WriteLine();
             }
 
             return objects;
         }
         
-        static void DrawObjects(char[,] objects,int length, int heigth, int ground, int playerPositionX, int playerPositionY)
+        static void DrawObjects(char[,] objects,int length, int heigth, int ground, ref int playerPositionX, ref int playerPositionY, int displayX, int displayY)
         {
+            System.Threading.Thread.Sleep(0);
+
+
+            Console.SetCursorPosition(displayX, displayY);
+
+            SetPlayerPosition(ref objects, playerPositionX, playerPositionY);
+
             Console.BackgroundColor = ConsoleColor.Blue;
 
             for (int y = 0; y < objects.GetLength(0) - ground; y++)
@@ -111,7 +122,7 @@ namespace brave
                 Console.WriteLine();
             }
 
-            Console.BackgroundColor = ConsoleColor.DarkGray;
+            Console.BackgroundColor = ConsoleColor.DarkGreen;
 
             for (int y = objects.GetLength(0) - ground; y < objects.GetLength(0); y++)
             {
@@ -125,17 +136,116 @@ namespace brave
 
             Console.ResetColor(); 
             
-            DrawPlayer(objects, playerPositionX, playerPositionY);
         }
 
-        static void DrawPlayer(char[,] objects, int positonX, int positionY)
+        static void SetPlayerPosition(ref char[,] objects, int positonX, int positionY)
         {
             objects[positionY, positonX] = '>';
         }
 
-        static void Shoot(char[,] objects, int positionX, int positionY)
+        static void Shoot(ref char[,] objects, int positionX, int positionY, int gameScreenLength, int gameScreenHeigth, int gameGroundHeigth, int displayX, int displayY)
         {
+            int projectileRange = 20;
+            int projectileX = positionX + 1;
+            objects[positionY, projectileX] = '=';
 
+            for (int x = 0; x < projectileRange; x++)
+            {
+                if (objects[positionY, projectileX] == '#')
+                {
+                    objects[positionY, projectileX] = ' ';
+                    break;
+                }
+
+                objects[positionY, projectileX] = '=';
+                objects[positionY, projectileX - 1] = ' ';
+                projectileX++;
+
+                DrawObjects(objects, gameScreenLength, gameScreenHeigth, gameGroundHeigth, ref positionX, ref positionY, displayX, displayY);
+              
+            }
+
+            objects[positionY, projectileX - 1] = ' ';
+        }
+
+        static void Jump(ref char[,] objects, int positionX, int positionY, int gameScreenLength, int gameScreenHeigth, int gameGroundHeigth, int displayX, int displayY)
+        {
+            int heigthJump = 5;
+
+            for(int y = 0; y < heigthJump; y++)
+            {
+                System.Threading.Thread.Sleep(50);
+
+                if (objects[positionY - 1, positionX] == ' ')
+                {
+                    positionY--;
+
+
+                    objects[positionY + 1, positionX] = ' ';
+
+
+                    DrawObjects(objects, gameScreenLength, gameScreenHeigth, gameGroundHeigth, ref positionX, ref positionY, displayX, displayY);
+                }
+
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo key = Console.ReadKey();
+
+                    switch (key.Key)
+                    {
+                        case ConsoleKey.Enter:
+
+                            Shoot(ref objects, positionX, positionY, gameScreenLength, gameScreenHeigth, gameGroundHeigth, displayX, displayY);
+
+                            break;
+                        case ConsoleKey.RightArrow:
+
+                            objects = GenerateNewObjects(objects, gameGroundHeigth, positionX, positionY);
+
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
+
+            for (int y = 0; y < heigthJump; y++)
+            {
+                System.Threading.Thread.Sleep(50);
+
+                if (objects[positionY + 1, positionX] == ' ')
+                {
+                    positionY++;
+
+
+                    objects[positionY - 1, positionX] = ' ';
+
+
+                    DrawObjects(objects, gameScreenLength, gameScreenHeigth, gameGroundHeigth, ref positionX, ref positionY, displayX, displayY);
+                }
+
+
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo key = Console.ReadKey();
+
+                    switch (key.Key)
+                    {
+                        case ConsoleKey.Enter:
+
+                            Shoot(ref objects, positionX, positionY, gameScreenLength, gameScreenHeigth, gameGroundHeigth, displayX, displayY);
+
+                            break;
+                        case ConsoleKey.RightArrow:
+
+                            objects = GenerateNewObjects(objects, gameGroundHeigth, positionX, positionY);
+
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
         }
     }
 }
